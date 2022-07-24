9d33f342d7200ce46177de55c9a49cfa77bdd46e
SFN <service.sfn+bitbucket@gmail.com>
2022-07-24 15:37:02 +0200

    + Tab-Seiten hinzugefügt/angepasst

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,19 +3,19 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="map" href="/tabs/map">
+          <ion-icon src="assets/icon/ic_home.svg" />
+          <ion-label>Suchen</ion-label>
         </ion-tab-button>
           
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="ausleihen" href="/tabs/ausleihen">
+          <ion-icon src="assets/icon/ic_bicycle.svg" />
+          <ion-label>Ausleihen</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="profil" href="/tabs/profil">
+          <ion-icon src="assets/icon/ic_account.svg" />
+          <ion-label>Profil</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -25,17 +25,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
-    return {
-      ellipse, 
-      square, 
-      triangle,
-    }
+    return {}
   }
 });
 </script>
