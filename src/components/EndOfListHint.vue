846a4dbe3b11e7286076e5002eec17cd854f8c9a
SFN <service.sfn+bitbucket@gmail.com>
2022-08-19 17:05:48 +0200

    + EndOfListHint Komponente

---
+++ src/components/EndOfListHint.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list-hint">
+    <p>{{ message }}</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {prop, Vue} from "vue-class-component";
+import "@/theme/variables.css";
+
+export class EndOfListHintProps {
+  static readonly DEFAULT_MESSAGE = "Keine weiteren Elemente";
+  message = prop<string>({required: false, default: EndOfListHintProps.DEFAULT_MESSAGE});
+}
+
+export default class EndOfListHint extends Vue.with(EndOfListHintProps) {
+
+}
+
+</script>
+
+<style scoped>
+.list-hint {
+  color: var(--ion-color-medium, grey);
+  text-align: center;
+  font-weight: lighter;
+  font-size: medium;
+  margin: 2em 2em;
+}
+</style>
